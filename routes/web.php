f12a0bf2902e5bf47185521605b66114f9c854a2
Nuuasli <wayanwisnu560@gmail.com>
2024-11-07 15:35:37 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+$pages = [
+    '/' => 'dashbord',
+    '/about' => 'about',
+    '/services' => 'services',
+    '/smClass' => 'classes',
+    '/trainer' => 'trainer',
+    '/gym' => 'gym',
+    '/membership' => 'membership',
+    '/contact' => 'contact'
+];
 
-Route::resource('/', 'App\Http\Controllers\HealthMateControler');
-Route::resource('/about', 'App\Http\Controllers\AboutControler');
-Route::resource('/services', 'App\Http\Controllers\ServiceControler');
-Route::resource('/smClass', 'App\Http\Controllers\ClassesControler');
-Route::resource('/trainer', 'App\Http\Controllers\TrainerControler');
-Route::resource('/gym', 'App\Http\Controllers\GymControler');
-Route::resource('/membership', 'App\Http\Controllers\MembershipControler');
-Route::resource('/contact', 'App\Http\Controllers\ContactControler');
+foreach ($pages as $uri => $view) {
+    Route::get($uri, function () use ($view) {
+        return view($view);
+    });
+}
+
+// controler
 Route::resource('/classes', 'App\Http\Controllers\smallclassControler');
 Route::resource('/member', 'App\Http\Controllers\memberControler');
